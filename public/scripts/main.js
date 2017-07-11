9e10b730282c324c19fc89f671c4bf31dadaf61d
Umi Syam <umi.syam@nytimes.com>
2017-07-11 14:54:10 -0400

    make TOC sticky

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
   $("pre").html(function (index, html) {
       return html.replace(/^(.*)$/mg, "<span class=\"line\">$1</span>")
   });
+
+  // make TOC sticky
+  var $toc = $(".g-left-panel");
+  var stickyTop = $toc.offset().top - 100;
+  $(window).on('scroll', function(){
+    ($(window).scrollTop() >= stickyTop) ? $toc.addClass('d-fixed') : $toc.removeClass('d-fixed');
+  });
 })
