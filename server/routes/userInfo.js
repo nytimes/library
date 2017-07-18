c7569ea960d3d331f56457a652b225791fdaac39
Ben Koski <bkoski@nytimes.com>
2017-07-17 21:51:52 -0400

    merge verifyIapToken setup into userInfo file; fix lint warnings

--- server/routes/userInfo.js
+++ server/routes/userInfo.js
@@ -1,24 +1,27 @@
 'use strict'
 
 const express = require('express')
+const {verifyIapToken} = require('***REMOVED***')
 const router = express.Router()
 const md5 = require('md5')
 
-// add userInfo to the req object
+router.use(verifyIapToken())
+
+// add userInfo to res.locals
 router.use((req, res, next) => {
   // In development, use data from an ENV var
-  if(process.env.TEST_EMAIL) {
+  if (process.env.TEST_EMAIL) {
     res.locals.userInfo = {
       email: process.env.TEST_EMAIL,
-      userId: "10",
+      userId: '10',
       analyticsUserId: md5('10library')
     }
   } else {
     // pluck data from headers set by iap-verify-middleware
     res.locals.userInfo = {
-      email:  req.headers['auth.verified_email'],
+      email: req.headers['auth.verified_email'],
       userId: req.headers['auth.verified_sub'],
-      analyticsUserId: md5(req.headers['auth.verified_sub'] +'library')
+      analyticsUserId: md5(req.headers['auth.verified_sub'] + 'library')
     }
   }
   next()
