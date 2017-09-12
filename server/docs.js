b74652d9fe62d64f9be75f908fe0f4c0beaaeb58
Isaac White <isaac.white@nytimes.com>
2017-09-12 11:35:27 -0400

    account for leading spaces in file/folder names

--- server/docs.js
+++ server/docs.js
@@ -15,6 +15,7 @@ const supportedTypes = new Set(['document', 'spreadsheet', 'text/html'])
 
 exports.cleanName = (name = '') => {
   return name
+    .trim()
     .replace(/^\d+[-–—_\s]*/, '') // remove leading numbers and delimiters
     .replace(/\|\s*([^|]+)$/i, '')
     .replace(/\W+home$/i, '')
