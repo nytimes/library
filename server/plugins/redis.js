037c7257abacb0724dd1e268c8c6fff55e46a23e
Andrew Fischer <afischer15@mac.com>
2018-07-06 13:52:47 -0400

    Pull redis logic into abstractable 'plugin'

---
+++ server/plugins/redis.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const cacheManager = require('cache-manager')
+const redisStore = require('cache-manager-ioredis')
+const log = require('../logger')
+
+// Provides get, set methods for a cache
+
+module.exports = process.env.REDIS_URI ? cacheManager.caching({
+  store: redisStore,
+  host: process.env.REDIS_URI,
+  password: process.env.REDIS_PASS,
+  port: 6379,
+  keyPrefix: 'nyt-library',
+  db: 0,
+  ttl: 0 // default ttl is infinite
+}) : cacheManager.caching({ store: 'memory' })
+
+  // if we are using a redis instance, listen for errors
+if (process.env.REDIS_URI) {
+  const redisClient = exports.store.getClient()
+  redisClient.on('error', (err) => log.error('ERROR FROM REDIS CLIENT:', err))
+}
