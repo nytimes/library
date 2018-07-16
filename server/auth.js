2a1df3d6d67e50324e08fe6ede811c43d5a632a8
Suzanne Wang <suzanneswang@gmail.com>
2018-07-16 18:29:50 -0400

    Use node auth

--- server/auth.js
+++ server/auth.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const inflight = require('inflight')
 const {google} = require('googleapis')
+const {auth} = require('google-auth-library')
 
 const log = require('./logger')
 
@@ -15,6 +16,28 @@ if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(__dirname, '.auth.json')
 }
 
+const keysEnvVar = process.env.GOOGLE_APPLICATION_CREDENTIALS
+if (!keysEnvVar) {
+  log.error('GOOGLE_APPLICATION_CREDENTIALS was not defined!')
+}
+const keys = JSON.parse(keysEnvVar)
+
+async function main() {
+  // load the JWT or UserRefreshClient from the keys
+  const client = auth.fromJSON(keys);
+  client.scopes = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/cloud-platform', 
+    'https://www.googleapis.com/auth/datastore'
+  ]
+  await client.authorize()
+
+  console.log(Object.keys(client))
+  // const url = `https://www.googleapis.com/dns/v1/projects/${keys.project_id}`;
+  // const res = await client.request({url});
+
+  // console.log(res)
+}
 // only public method, returns the authClient that can be used for making other requests
 exports.getAuth = (cb) => {
   if (authClient) {
@@ -24,27 +47,40 @@ exports.getAuth = (cb) => {
   setAuthClient(cb)
 }
 
+
 // configures the auth client if we don't already have one
-function setAuthClient(cb) {
+async function setAuthClient(cb) {
   cb = inflight('auth', cb)
   // guard against calling while already in progress
   if (!cb) return
+  
+  const client = auth.fromJSON(keys);
+  client.scopes = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/cloud-platform', 
+    'https://www.googleapis.com/auth/datastore'
+  ]
+  await client.authorize()
 
-  google.auth.getApplicationDefault((err, client) => {
-    if (err) {
-      return cb(err)
-    }
+  cb(null, client)
 
-    authClient = client
-    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-      authClient = authClient.createScoped([
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/cloud-platform',
-        'https://www.googleapis.com/auth/datastore'
-      ])
-    }
-    google.options({auth: authClient})
-    log.info('Google API auth successfully retrieved.')
-    cb(null, authClient)
-  })
+  // google.auth.getApplicationDefault((err, client) => {
+  //   if (err) {
+  //     return cb(err)
+  //   }
+
+  //   authClient = client
+  //   if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+  //     authClient = authClient.createScoped([
+  //       'https://www.googleapis.com/auth/drive',
+  //       'https://www.googleapis.com/auth/cloud-platform',
+  //       'https://www.googleapis.com/auth/datastore'
+  //     ])
+  //   }
+  //   google.options({auth: authClient})
+  //   log.info('Google API auth successfully retrieved.')
+  //   cb(null, authClient)
+  // })
 }
+
+main().catch(console.error)
