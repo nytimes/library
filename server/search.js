99d488d142a1a84eb0b4225a3f14fcf2d56b1d41
Suzanne Wang <suzanneswang@gmail.com>
2018-07-17 17:53:31 -0400

    Implement search with shared drive

--- server/search.js
+++ server/search.js
@@ -3,7 +3,9 @@
 const {google} = require('googleapis')
 const {getAuth} = require('./auth')
 const list = require('./list')
-const teamDriveId = '***REMOVED***'
+
+const driveType = process.env.DRIVE_TYPE
+const driveId = process.env.DRIVE_ID
 
 exports.run = (query, cb) => {
   getAuth((err, authClient) => {
@@ -25,15 +27,27 @@ exports.run = (query, cb) => {
   })
 }
 
-function fullSearch({drive, query, results = [], next}, cb) {
-  const options = {
-    teamDriveId,
+function getOptions(driveType, id, query) {
+  const baseOptions = {
     q: `fullText contains ${JSON.stringify(query)} AND mimeType != 'application/vnd.google-apps.folder' AND trashed = false`,
+    fields: '*'
+  }
+
+  if (driveType === 'shared') {
+    return baseOptions
+  } 
+  
+  return {
+    teamDriveId: id,
     corpora: 'teamDrive',
     supportsTeamDrives: true,
     includeTeamDriveItems: true,
-    fields: '*'
+    ...baseOptions
   }
+}
+
+function fullSearch({drive, query, results = [], next}, cb) {
+  const options = getOptions(driveType, driveId, query)
 
   drive.files.list(options, (err, {data: {files, nextPageToken: next}}) => {
     if (err) return cb(err)
