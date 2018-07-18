307fd9f35283b6b2f010d74de8ea65a222348aaf
Suzanne Wang <suzanneswang@gmail.com>
2018-07-18 15:20:43 -0400

    Add conditional for using datastore

--- server/index.js
+++ server/index.js
@@ -53,7 +53,9 @@ app.get('/view-on-site/:docId', (req, res, next) => {
 })
 
 // main pages
-// app.use(readingHistory.middleware)
+if (!process.env.HEROKU) {
+  app.use(readingHistory.middleware)
+}
 
 // don't cache pages client-side to ensure browser always gets latest revision
 app.use((req, res, next) => {
