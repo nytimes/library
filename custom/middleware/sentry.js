fc009a9bad48e2c9dab4b422c5bf3f9808c404b9
Suzanne Wang <suzanneswang@gmail.com>
2018-07-24 10:26:17 -0400

    Remove custom middleware

--- custom/middleware/sentry.js
+++ custom/middleware/sentry.js
@@ -1,17 +1,17 @@
-'use strict'
+// 'use strict'
 
-const raven = require('raven')
+// const raven = require('raven')
 
-function initSentry() {
-  raven.config(process.env.SENTRY_DSN).install()
-  return raven.requestHandler()
-}
+// function initSentry() {
+//   raven.config(process.env.SENTRY_DSN).install()
+//   return raven.requestHandler()
+// }
 
-// error functions are special. They have to be attached directly to the app.
-exports.preload = process.env.SENTRY_DSN
-  ? initSentry()
-  : (res, req, next) => next() // pass if no sentry env variable
+// // error functions are special. They have to be attached directly to the app.
+// exports.preload = process.env.SENTRY_DSN
+//   ? initSentry()
+//   : (res, req, next) => next() // pass if no sentry env variable
 
-exports.postload = process.env.SENTRY_DSN
-  ? raven.errorHandler()
-  : (res, req, next) => next() // pass if no sentry env variable
+// exports.postload = process.env.SENTRY_DSN
+//   ? raven.errorHandler()
+//   : (res, req, next) => next() // pass if no sentry env variable
--- custom/userAuth.js
+++ custom/userAuth.js
@@ -1,30 +1,30 @@
-'use strict'
+// 'use strict'
 
-const express = require('express')
-const md5 = require('md5')
+// const express = require('express')
+// const md5 = require('md5')
 
-const {verifyIapToken} = require('***REMOVED***')
+// const {verifyIapToken} = require('***REMOVED***')
 
-const router = express.Router()
+// const router = express.Router()
 
-router.use(verifyIapToken())
+// router.use(verifyIapToken())
 
-router.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'development') {
-    req.userInfo = {
-      email: process.env.TEST_EMAIL || '***REMOVED***',
-      userId: '10',
-      analyticsUserId: md5('10library')
-    }
-    next()
-    return
-  }
-  req.userInfo = {
-    email: req.headers['auth.verified_email'],
-    userId: req.headers['auth.verified_sub'],
-    analyticsUserId: md5(req.headers['auth.verified_sub'] + 'library')
-  }
-  next()
-})
+// router.use((req, res, next) => {
+//   if (process.env.NODE_ENV === 'development') {
+//     req.userInfo = {
+//       email: process.env.TEST_EMAIL || '***REMOVED***',
+//       userId: '10',
+//       analyticsUserId: md5('10library')
+//     }
+//     next()
+//     return
+//   }
+//   req.userInfo = {
+//     email: req.headers['auth.verified_email'],
+//     userId: req.headers['auth.verified_sub'],
+//     analyticsUserId: md5(req.headers['auth.verified_sub'] + 'library')
+//   }
+//   next()
+// })
 
-module.exports = router
+// module.exports = router
