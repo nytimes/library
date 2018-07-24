2babbce3401938bc974085daea21ec81b00004fe
Suzanne Wang <suzanneswang@gmail.com>
2018-07-24 12:11:59 -0400

    Refactor auth env var detection

--- server/auth.js
+++ server/auth.js
@@ -11,11 +11,15 @@ const log = require('./logger')
 
 let authClient = null
 
+// Look for google application credentials as json env var
+const credsInJSON = isJSON(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
 // In local development, look for an auth.json file.
 if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   log.warn('GOOGLE_APPLICATION_CREDENTIALS was undefined, using default ./auth.json credentials file...')
   process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(__dirname, '.auth.json')
-} 
+}
+
 
 // only public method, returns the authClient that can be used for making other requests
 exports.getAuth = (cb) => {
@@ -36,17 +40,12 @@ async function setAuthClient(cb) {
 
   return inflight('auth', async () => {
     // In Heroku environment, set GOOGLE_APPLICATION_CREDENTIALS as auth json object to be parsed
-    if (process.env.HEROKU) {
-      const keysEnvVar = process.env.GOOGLE_APPLICATION_CREDENTIALS
-      if (!keysEnvVar) {
-        log.error('GOOGLE_APPLICATION_CREDENTIALS was not defined. Set the config var object in Heroku.')
-      }
-      const keys = JSON.parse(keysEnvVar)
+    if (credsInJSON) {
+      const keys = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
       authClient = nodeAuth.fromJSON(keys);
       authClient.scopes = scopes
 
       await authClient.authorize()
-      cb(null, authClient)
 
     } else {
       const getGoogleAuth = promisify(google.auth.getApplicationDefault).bind(google.auth)
@@ -57,9 +56,19 @@ async function setAuthClient(cb) {
         authClient = authClient.createScoped(scopes)
       }
       google.options({auth: authClient})
-      cb(null, authClient)
     }
 
+    cb(null, authClient)
     log.info('Google API auth successfully retrieved.')
   })
+}
+
+function isJSON(str) {
+  try {
+    const obj = JSON.parse(str)
+    if (obj && typeof obj === 'object' && obj !== null) {
+        return true
+      }
+  } catch (err) {}
+    return false
 }
--- server/routes/userInfo.js
+++ server/routes/userInfo.js
@@ -4,7 +4,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/whoami.json', (req, res, next) => {
-  console.log('req', req.userInfo)
   res.json(req.userInfo)
 })
 
