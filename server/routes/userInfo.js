4e3dc9270c2bfe91a152bface15b508572940f38
Suzanne Wang <suzanneswang@gmail.com>
2018-07-24 11:25:10 -0400

    Add line to debug userinfo

--- server/routes/userInfo.js
+++ server/routes/userInfo.js
@@ -4,6 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/whoami.json', (req, res, next) => {
+  console.log('req', req.userInfo)
   res.json(req.userInfo)
 })
 
