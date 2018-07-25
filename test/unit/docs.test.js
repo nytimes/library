8b514cf9924e344b405a6c550f26fbf7a657041c
Andrew Fischer <afischer15@mac.com>
2018-07-25 17:42:19 -0400

    Stubbing and testing for docs, yay coverage

--- test/unit/docs.test.js
+++ test/unit/docs.test.js
@@ -1,12 +1,53 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
 const {expect} = require('chai')
 const {google} = require('googleapis')
 
+const {multisectionHTML} = require('../fixtures/multisectionHTML')
+
+// Google drive response stubbing spagetti
 google.auth.getApplicationDefault = () => {
   return {credential: {JWT: {}}}
 }
 google.options = () => {}
+google.drive = () => {
+  return {
+    files: {
+      'export': (opts) => {
+        if (opts.mimeType.includes('spreadsheetml')) {
+          return {
+            data: fs.readFileSync(path.join(__dirname, '../fixtures/sheet-buffer'))
+          }
+        }
+        if (opts.fileId === 'mulitsection') {
+          return {data: multisectionHTML}
+        }
+        return {data: `<html><head><meta content="text/html; charset=UTF-8" http-equiv="content-type"></head><body style="background-color:#ffffff;padding:72pt 72pt 72pt 72pt;max-width:468pt"><p style="padding:0;margin:0;color:#000000;font-size:11pt;font-family:&quot;Arial&quot;;line-height:1.15;orphans:2;widows:2;text-align:left"><span style="color:#000000;font-weight:400;text-decoration:none;vertical-align:baseline;font-size:11pt;font-family:&quot;Arial&quot;;font-style:normal">This is a simple test document export.</span></p></body></html>`}
+      },
+      'get': () => {
+        return {data: '<h1>This is a raw HTML document</h1>'}
+      }
+    },
+    revisions: {
+      get: () => {
+        return Promise.resolve({ data: {
+          kind: 'drive#revision',
+          mimeType: 'application/vnd.google-apps.document',
+          modifiedTime: '2017-01-01T19:55:07.353Z',
+          published: false,
+          lastModifyingUser: {
+            kind: 'drive#user',
+            displayName: 'John Smith',
+            photoLink: 'https://foo.com/photo.jpg',
+            me: false
+          }
+        }})
+      }
+    }
+  }
+}
 
 const docs = require('../../server/docs')
 
@@ -69,8 +110,77 @@ describe('Docs', () => {
     })
   })
 
-  // describe('Fetching Docs', () => {
-  //   '1dUMaK6q_EhjwQp9P5NaPfiLY1vJVt4CTz6Gnm9sPPOY'
-  //   'document'
-  // })
+  describe('Fetching Docs', () => {
+    it('should successully fetch a document data', async () => {
+      const doc = await docs.fetchDoc('id1', 'document', {})
+      expect(doc).to.include.keys('html', 'originalRevision')
+    })
+
+    it('should get correct revision data', async () => {
+      const {originalRevision} = await docs.fetchDoc('id1', 'document', {})
+      expect(originalRevision.data).to.have.keys('kind', 'mimeType', 'modifiedTime', 'published', 'lastModifyingUser')
+    })
+
+    it('should have correct mimetype for document', async () => {
+      const {originalRevision} = await docs.fetchDoc('id1', 'document', {})
+      const {mimeType} = originalRevision.data
+      expect(mimeType).equals('application/vnd.google-apps.document')
+    })
+
+    it('should parse sections correctly', async () => {
+      const doc = await docs.fetchDoc('mulitsection', 'document', {})
+      expect(doc).to.include.keys('html', 'sections')
+      const {sections} = doc
+      expect(sections.length).equals(2)
+      expect(sections[0].subsections.length).equals(3)
+    })
+  })
+
+  describe('Fetching Sheets', () => {
+    it('should successully fetch a document data', async () => {
+      const doc = await docs.fetchDoc('id1', 'document', {})
+      expect(doc).to.include.keys('html', 'originalRevision')
+    })
+
+    it('should get correct revision data', async () => {
+      const {originalRevision} = await docs.fetchDoc('id1', 'document', {})
+      expect(originalRevision.data).to.have.keys('kind', 'mimeType', 'modifiedTime', 'published', 'lastModifyingUser')
+    })
+
+    it('should have correct mimetype for document', async () => {
+      const {originalRevision} = await docs.fetchDoc('id1', 'document', {})
+      const {mimeType} = originalRevision.data
+      expect(mimeType).equals('application/vnd.google-apps.document')
+    })
+  })
+
+  describe('Fetching Sheets', () => {
+    it('should successully fetch sheet data', async () => {
+      const sheet = await docs.fetchDoc('id1', 'spreadsheet', {})
+      expect(sheet).to.include.keys('html', 'originalRevision')
+    })
+
+    it('should successully parse the sheet to a html table', async () => {
+      const {html} = await docs.fetchDoc('id1', 'spreadsheet', {})
+      expect(html).includes('<table>')
+      expect(html).includes('</table>')
+    })
+  })
+
+  describe('Fetching html', () => {
+    it('should successully fetch html', async () => {
+      const sheet = await docs.fetchDoc('id1', 'text/html', {})
+      expect(sheet).to.include.keys('html', 'originalRevision')
+    })
+
+    it('should not modify html', async () => {
+      const {html} = await docs.fetchDoc('id1', 'text/html', {})
+      expect(html).equals('<h1>This is a raw HTML document</h1>')
+    })
+  })
+
+  it('should identify bad resource types', async () => {
+    const {html} = await docs.fetchDoc('id1', 'badtype', {})
+    expect(html).equals('Library does not support viewing badtypes yet.')
+  })
 })
