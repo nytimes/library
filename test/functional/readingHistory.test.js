0a0674797d6707e19c0d5b5dd3950b9689f8e9e0
Andrew Fischer <afischer15@mac.com>
2018-07-26 18:17:42 -0400

    Datastore stubbing and simple reading history tests

--- test/functional/readingHistory.test.js
+++ test/functional/readingHistory.test.js
@@ -4,7 +4,6 @@
 
 const request = require('supertest')
 const {expect} = require('chai')
-const express = require('express')
 
 const app = require('../../server/index')
 
@@ -19,15 +18,40 @@ describe('Reading History', () => {
   before(() => {
     app.request.session = {passport: {user: userInfo}}
   })
-  // it('GET /reading-history/docs.json', (done) => {
-  //   request(app)
-  //     .get('/reading-history/docs.json')
-  //     // .expect(200)
-  //     .end((err, res) => {
-  //       console.log(err);
-  //       if (err) return done(err)
-  //       console.log(res.text);
-  //       done()
-  //     })
-  // })
+
+  it('GET /reading-history/docs.json succeeds', (done) => {
+    request(app)
+      .get('/reading-history/docs.json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        const json = JSON.parse(res.text)
+        expect(json).to.have.keys('recentlyViewed', 'mostViewed')
+        done()
+      })
+  })
+
+  it('GET /reading-history/docs.json has correct docs', (done) => {
+    request(app)
+      .get('/reading-history/docs.json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        const {recentlyViewed, mostViewed} = JSON.parse(res.text)
+        const recentIds = recentlyViewed.map((obj) => obj.documentId)
+        const mostIds = mostViewed.map((obj) => obj.documentId)
+        expect(recentIds).to.include(
+          'xxxxxJ7S71V0K0z_P6XvDkdh4aIYO8AbMeVjfXDxxxxxJFbiE',
+          'xxxxxZjvhyI8uWoQpCFRmdLrLc5yyD1sjEVCkFrxxxxxdU6JQ',
+          'xxxxxhd7b-l4h2N3JfjOtxkudx1Zs0M9g09RporxxxxxBq6z8',
+          'xxxxxlLU3WQO9D_BmsCHu0R8teQwiRfNRPRl8AlxxxxxjaVts'
+        )
+        expect(mostIds).to.include(
+          'xxxxxJ7S71V0K0z_P6XvDkdh4aIYO8AbMeVjfXDxxxxxJFbiE',
+          'xxxxxZjvhyI8uWoQpCFRmdLrLc5yyD1sjEVCkFrxxxxxdU6JQ',
+          'xxxxxhd7b-l4h2N3JfjOtxkudx1Zs0M9g09RporxxxxxBq6z8'
+        )
+        done()
+      })
+  })
 })
--- test/utils/bootstrap.js
+++ test/utils/bootstrap.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const {google} = require('googleapis')
+let datastore = require('@google-cloud/datastore')
+
 const express = require('express')
 
 const {initMocks} = require('../utils/googleMock')
@@ -26,4 +28,83 @@ express.request.userInfo = {
   email: 'test.user@test.com',
   userId: '10',
   analyticsUserId: 'asdfjkl123library'
+}
+
+datastore.prototype.key = () => {}
+datastore.prototype.runQuery = (opts) => {
+  if (opts[0] === 'LibraryViewDoc') {
+    return [
+      [
+        {
+          email: 'test.user@nytimes.com',
+          userId: '10',
+          lastViewedAt: '2018-07-25T19:54:07.308Z',
+          viewCount: 174,
+          documentId: 'xxxxxwY_gHO6hpoUBxPlbTY6iHihxxxxxLiHWm'
+        },
+        {
+          email: "test.user@nytimes.com",
+          userId: "10",
+          lastViewedAt: "2018-07-25T18:45:35.607Z",
+          viewCount: 121,
+          documentId: "xxxxxjwpW5uRxJmpCITNTw9CNupAsFmm4gUkEXsxxxxx_"
+        },
+        {
+          lastViewedAt: "2018-07-11T14:11:39.409Z",
+          viewCount: 118,
+          documentId: "xxxxxXF8YewPwFbMfubvVP-oNHpFwHLtkd3Je1hxxxxxhWhlM",
+          email: "test.user@nytimes.com",
+          userId: "10"
+        },
+        {
+          viewCount: 83,
+          documentId: "xxxxx-mJbaYIzAM0_PEPOEQl55TikHKQZKI9Ji3xxxxxDAo7E",
+          email: "test.user@nytimes.com",
+          userId: "10",
+          lastViewedAt: "2018-07-25T15:43:28.334Z"
+        }
+      ],
+      {
+        moreResults: "MORE_RESULTS_AFTER_LIMIT",
+        endCursor: "asdfasdfadsfadsf"
+      }
+    ]
+  } else {
+    return [
+      [
+        {
+          email: "test.user@nytimes.com",
+          userId: "10",
+          lastViewedAt: "2018-07-25T21:15:27.429Z",
+          viewCount: 8,
+          documentId: "xxxxxJ7S71V0K0z_P6XvDkdh4aIYO8AbMeVjfXDxxxxxJFbiE"
+        },
+        {
+          email: "test.user@nytimes.com",
+          userId: "10",
+          lastViewedAt: "2018-07-25T20:45:37.014Z",
+          viewCount: 36,
+          documentId: "xxxxxZjvhyI8uWoQpCFRmdLrLc5yyD1sjEVCkFrxxxxxdU6JQ"
+        },
+        {
+          lastViewedAt: "2018-07-25T20:23:08.840Z",
+          viewCount: 10,
+          documentId: "xxxxxhd7b-l4h2N3JfjOtxkudx1Zs0M9g09RporxxxxxBq6z8",
+          email: "test.user@nytimes.com",
+          userId: "10"
+        },
+        {
+          email: "test.user@nytimes.com",
+          userId: "10",
+          lastViewedAt: "2018-07-25T20:15:03.458Z",
+          viewCount: 1,
+          documentId: "xxxxxlLU3WQO9D_BmsCHu0R8teQwiRfNRPRl8AlxxxxxjaVts"
+        }
+      ],
+      {
+        moreResults: "MORE_RESULTS_AFTER_LIMIT",
+        endCursor: "asdfasdfasdfadsf"
+      }
+    ]
+  }
 }
