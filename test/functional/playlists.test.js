3afb18094804341e1911c47aecbad212672543b1
Suzanne Wang <suzanneswang@gmail.com>
2018-08-09 21:35:29 -0400

    Add html checks

--- test/functional/playlists.test.js
+++ test/functional/playlists.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const sinon = require('sinon')
+const {expect} = require('chai')
 
 const app = require('../../server/index')
 const {getTree, getDocsInfo} = require('../../server/list')
@@ -21,13 +22,13 @@ describe('Playlist route handling', () => {
     return request(app)
             .get(`${playlistPath}/article-dnecn`)
             .expect(200)
-            .then((res) => {console.log(res.text)})
+            
   })
 
   it('should query a playlist', () => {
     return request(app)
             .get(`${playlistPath}`)
             .expect(200)
-            .then((res) => {console.log(res.text)})
+            .then((res) => expect(res.text).to.contain('<h1 class="visually-hidden">Default Playlist template</h1>'))
   })
 })
