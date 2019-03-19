5c08c23add3964aada2b1a48e49952cd3d5f84df
Isaac White <isaac.white@nytimes.com>
2019-03-19 10:08:52 -0400

    update supported formats doc

--- test/utils/updateSupportedFormats.js
+++ test/utils/updateSupportedFormats.js
@@ -5,7 +5,7 @@ const {google} = require('googleapis')
 const {getAuth} = require('../../server/auth')
 require('dotenv').config()
 
-const TEST_FILE_ID = '10o-sZt7kzP-GZDEFrNbfwBy7hFe1toNgEVH2QdQSZ5s'
+const TEST_FILE_ID = '12bUE9b4_aF_IfhxCLdHApN8KKXwa4gQUCGkHzt1FyRI'
 
 // Run this from root of project as "node test/utils/updateSupportedFormats.js"
 // to refresh the contents of supported_formats.html
@@ -20,15 +20,6 @@ async function updateSupportedFormats() {
     if (err) { return console.log('Failed fetching v3 file!', err) }
     fs.writeFileSync('test/fixtures/supportedFormats.html', data, { encoding: 'utf8' })
     console.log('Successfully fetched v3 html export.')
-  })
-
-  google.discoverAPI(`***REMOVED***${process.env.API_KEY}`).then((docs) => {
-    docs.documents.get({ name: `documents/${TEST_FILE_ID}` }, (err, {data}) => {
-      if (err) { return console.log('Failed fetching v4 file!', err) }
-      const json = JSON.stringify(data)
-      fs.writeFileSync('test/fixtures/supportedFormatsV4.json', json, { encoding: 'utf8' })
-      console.log('Successfully fetched v4 json payload.')
-    })
   })
 }
 
