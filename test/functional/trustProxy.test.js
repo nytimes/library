99d7d32167770ccc94d3daf3ebcff647d6d7c83c
Isaac White <isaac.white@nytimes.com>
2019-10-19 21:27:48 -0400

    for now, remove decache

--- test/functional/trustProxy.test.js
+++ test/functional/trustProxy.test.js
@@ -4,33 +4,23 @@ const request = require('supertest')
 const {assert} = require('chai')
 const sinon = require('sinon')
 const express = require('express')
-const decache = require('decache')
-
-const userInfo = {
-  emails: [{value: 'test.user@test.com'}],
-  id: '10',
-  userId: '10',
-  _json: {domain: 'test.com'}
-}
 
 describe('Trust Proxy', () => {
   describe('when trust proxy env is not true', () => {
     before(() => sinon.stub(express.request, 'isAuthenticated').returns(false))
-    beforeEach(() => decache('../../server/index'))
+    beforeEach(() => delete require.cache[require.resolve('../../server/index')])
     after(() => sinon.restore())
     // app moved inside tests so that it can be recreated later with different
     // environment variables setup.
     const app = require('../../server/index')
 
-
     it('redirect should be for http with no headers', () => {
-
       return request(app)
         .get('/login')
         .expect(302) // expect user to be found
         .then((res) => {
           assert(res.redirect)
-          assert.include(res.headers.location, "redirect_uri=http%3A",'Redirect URI is http')
+          assert.include(res.headers.location, 'redirect_uri=http%3A', 'Redirect URI is http')
         })
     })
 
@@ -41,36 +31,33 @@ describe('Trust Proxy', () => {
         .expect(302) // expect user to be found
         .then((res) => {
           assert(res.redirect)
-          assert.include(res.headers.location, "redirect_uri=http%3A",'Redirect URI is http')
+          assert.include(res.headers.location, 'redirect_uri=http%3A', 'Redirect URI is http')
         })
     })
-    //app.server.close()
+    // app.server.close()
   })
 
   describe('when trust proxy env is set to true', () => {
     before(() => sinon.stub(express.request, 'isAuthenticated').returns(false))
-    beforeEach(() => decache('../../server/index'))
+    beforeEach(() => delete require.cache[require.resolve('../../server/index')])
     after(() => sinon.restore())
     // app moved inside tests so that it can be recreated later with different
     // environment variables setup.
 
-
-
-
     it('redirect should be for http with no headers', () => {
-      process.env.TRUST_PROXY = "true"
+      process.env.TRUST_PROXY = 'true'
       const app = require('../../server/index')
       return request(app)
         .get('/login')
         .expect(302) // expect user to be found
         .then((res) => {
           assert(res.redirect)
-          assert.include(res.headers.location, "redirect_uri=http%3A",'Redirect URI is http')
+          assert.include(res.headers.location, 'redirect_uri=http%3A', 'Redirect URI is http')
         })
     })
 
     it('redirect should be for https with headers and env set', () => {
-      process.env.TRUST_PROXY = "true"
+      process.env.TRUST_PROXY = 'true'
       const app = require('../../server/index.js')
       return request(app)
         .get('/login')
@@ -78,10 +65,8 @@ describe('Trust Proxy', () => {
         .expect(302) // expect user to be found
         .then((res) => {
           assert(res.redirect)
-          assert.include(res.headers.location, "redirect_uri=https%3A",'Redirect URI is https')
+          assert.include(res.headers.location, 'redirect_uri=https%3A', 'Redirect URI is https')
         })
     })
   })
-
-
 })
