85cb75e801c7a1dadce96a36b6b429305a8e0597
ahinh43 <alec4555@gmail.com>
2022-04-26 08:41:34 -0500

    Rename some variables, add description to variables

--- ops/terraform/modules/appengine/variables.tf
+++ ops/terraform/modules/appengine/variables.tf
@@ -1,18 +1,27 @@
 variable "project_id" {
+  description = "The project ID to create resources in. Required"
   type    = string
 }
 
-variable "client_id" {
-  type    = string
+
+# Unsure what these are for, not used so will be disabling for now
+# variable "client_id" {
+#   type    = string
+# }
+
+
+# variable "client_secret" {
+#   type    = string
+# }
+
+
+variable "region" {
+  description = "The region ID to place the application in. Required"
+  type = string
 }
-
-
-variable "client_secret" {
-  type    = string
-}
-
 
 variable "database_type" {
-  type    = string
-  default = "CLOUD_DATASTORE_COMPATIBILITY"
+  description = "Type of cloud firestore or cloud datastore database associated with the application. Set to either CLOUD_FIRESTORE or CLOUD_DATASTORE_COMPATIBILITY. Defaults to CLOUD_DATASTORE_COMPATIBILITY"
+  type        = string
+  default     = "CLOUD_DATASTORE_COMPATIBILITY"
 }
