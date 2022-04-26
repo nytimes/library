da2baf5adebfd6dc1ed213dccfde33ae6fbc23d6
ahinh43 <alec4555@gmail.com>
2022-04-26 15:14:27 -0500

    Create and wire up datastore

---
+++ ops/terraform/dev/us-east1/00_datastore_LibraryViewTeam_userLastViewedAt copy/terragrunt.hcl
@@ -0,0 +1,28 @@
+# Terraform module call to configure datastore for LibraryViewDoc Entity kind
+terraform {
+   source = "${get_parent_terragrunt_dir()}//modules//datastore"
+}
+
+include {
+   path = find_in_parent_folders()
+}
+
+locals {
+  environment = "dev"
+  resource_prefix = "nytimes-library"
+}
+
+inputs = {
+  entity_kind = "LibraryViewTeam"
+  properties = tolist([
+    {
+      name = "userId"
+      direction = "ASCENDING"
+    },
+    {
+      name = "lastViewedAt"
+      direction = "DESCENDING"
+    }
+  ])
+  
+}
--- ops/terraform/modules/datastore/main.tf
+++ ops/terraform/modules/datastore/main.tf
@@ -1,11 +1,12 @@
-resource "google_datastore_index" "default" {
-  kind = "foo"
-  properties {
-    name      = "property_a"
-    direction = "ASCENDING"
-  }
-  properties {
-    name      = "property_b"
-    direction = "ASCENDING"
+# Creates the datastore indexes needed for the application to function
+resource "google_datastore_index" "main" {
+  kind = var.entity_kind
+
+  dynamic "properties" {
+    for_each = var.properties[*]
+    content {
+      name = lookup(auth.value, "name", null)
+      direction = lookup(auth.value, "direction", null)
+    }
   }
 }
