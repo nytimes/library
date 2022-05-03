d13e3ac330f6fcec7481b5e8d1d64849c04a67ec
ahinh43 <alec4555@gmail.com>
2022-05-03 15:25:21 -0500

    Disable terraform IAP client and add infra documentation

--- ops/terraform/modules/library_oauth_client/main.tf
+++ ops/terraform/modules/library_oauth_client/main.tf
@@ -1,9 +1,14 @@
+# GCP API is VERY limited in what you can create for OAuth. Most of the steps needed to deploy the OAuth configuration must be done on console.
 resource "google_iap_brand" "main" {
   support_email     = var.support_email
   application_title = var.application_title
 }
 
-resource "google_iap_client" "main" {
-  display_name = var.oauth_client_name
-  brand        =  google_iap_brand.main.name
-}
+# Disabling this, what happens is Terraform creates a barebones oauth client that you can't even access on the console
+# Which also means you can't create authorized redirect URIs, javascript origins, etc. It's basically a useless barebones client.
+# resource "google_iap_client" "main" {
+#   display_name = var.oauth_client_name
+#   brand        =  google_iap_brand.main.name
+# }
+
+# A Oauth client must be created manually on the console or via gcloud commands?
