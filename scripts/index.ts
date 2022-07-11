9b6483545168638f191198936223f7f6f59e6348
Nick Sayre <nick.sayre@adhocteam.us>
2022-07-11 16:48:44 -0500

    Handle see more/less with Stimulus instead of jQuery

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,11 +3,11 @@ import UsefulnessController from './controllers/usefulness_controller'
 
 // Import controllers here
 // Example:
-// import HelloController from "./controllers/hello_controller"
+import ChildrenListController from "./controllers/children_list_controller"
 
 const application = Application.start()
 
 // Register controllers here
 // Example:
-// application.register("hello", HelloController)
+application.register("children-list", ChildrenListController)
 application.register('usefulness', UsefulnessController)
