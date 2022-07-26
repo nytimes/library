a4e6d91abd3e4d785dbdc133dfaa3da620bc1f63
Nick Sayre <nick.sayre@adhocteam.us>
2022-07-26 14:29:51 -0500

    Move code that stores filenames in local storage to Stimulus

--- scripts/controllers/search_controller.ts
+++ scripts/controllers/search_controller.ts
@@ -12,6 +12,29 @@ class SearchController extends Controller {
 
   connect() {
     this.keypress = false
+
+    // request latest filenames, req will 304 and serve cache if unchanged
+    var lastFilenameFetch = this.#getFilenameStorage()
+    var now = new Date().getTime()
+    if (!lastFilenameFetch || now - lastFilenameFetch.lastFetched > 600000) { // 10 min
+      fetch(
+        '/filename-listing.json',
+        {
+          method: 'GET',
+        }
+      )
+      .then((response) =>{
+          if (response.status !== 200) return; // if request fails, continue with old listing
+          return response.json()
+        }
+      )
+      .then(response => {
+        var lastFetched = new Date().getTime()
+        var uniqueNames = new Set(response.filenames)
+        var data = {lastFetched: lastFetched, filenames: Array.from(uniqueNames)}
+        localStorage.setItem('filenames', JSON.stringify(data))
+      });
+    }
   }
 
   // Re/generate suggested results
