790b00833e8de2a1a69bdb5d7a779e16bd61e3b9
Nolan Shaffer <nshaff3r@Nolans-MacBook-Air.local>
2024-11-09 01:42:27 +0530

    bug fix for vercel

--- server/cache.js
+++ server/cache.js
@@ -7,7 +7,7 @@ const log = require('./logger')
 const {requireWithFallback} = require('./utils')
 const {parseUrl} = require('./urlParser')
 
-const cache = requireWithFallback('cache/store')
+const cache = requireWithFallback('./cache/store')
 
 // delay caching for 1 hour by default after editing, with env var override
 const noCacheDelay = parseInt(process.env.EDIT_CACHE_DELAY, 10) || 60 * 60
